27dc0af1f908ec79505d6310ac86f99ebc35db77
thronecoder <johnkiribata12@gmail.com>
2025-07-11 15:50:55 +0300

    docs(swagger): Set up swagger documentation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,14 @@
+// @title secSafe API
+// @version 1.0
+// @description This is the API documentation for secSafe
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name John Kiribata
+// @contact.url https://github.com/thronecoder/secsafe-backend
+// @contact.email johnkiribata12@gmail.com
+
+// @host localhost:8080
+// @BasePath /
 package main
 
 import (
@@ -6,7 +17,23 @@ import (
 	"github.com/thronecoder/secsafe-backend/internal/common"
 
 	"github.com/gin-gonic/gin"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "github.com/thronecoder/secsafe-backend/api/docs"
 )
+
+// PingExample godoc
+// @Summary      Ping the server
+// @Description  Check if server is alive
+// @Tags         Health
+// @Accept       json
+// @Produce      json
+// @Success      200  {object}  map[string]string
+// @Router       /ping [get]
+func PingHandler(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "pong"})
+}
 
 func main() {
 	cfg := common.LoadConfig()
@@ -16,9 +43,9 @@ func main() {
 
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
-	})
+	//Register Swagger endpoint
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.GET("/ping", PingHandler)
 
 	r.Run(fmt.Sprintf(":%s", cfg.AppPort))
 }
